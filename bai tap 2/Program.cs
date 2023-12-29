3f1303e9727f22a7e554973e1e719f267c0a25d9
ADMIN <ADMIN@PC333>
2023-12-29 11:30:50 +0700

    Add project files.

---
+++ bai tap 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_tap_2
+{
+    class  Account
+    {
+        private string id;
+        private string name;
+        private int balance;
+    
+    public Account(string id,string name,int balance)
+    {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+        public string getid()
+        {
+            return id;
+        }
+        public string getname()
+        {
+            return name;
+        }
+        public int getbalance()
+        {
+            return balance;
+        }
+        public void credit(int amount)
+        {
+            if(balance>0)
+            balance += amount; 
+        }
+        public void debit(int amount)
+        {
+            if(amount>0)
+            balance -= amount;
+        }
+        public void tranferTo(Account acc ,int amount)
+        {
+            if(balance >0)
+            balance -= amount;
+            acc.balance += amount;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Account my = new Account("12345678910", "TuanLuuDan", 4000);
+                Account myfriend = new Account("789123456", "TuThuocNo", 2000);
+
+                Console.WriteLine("------Thông Tin Tài Khoảng Ban Đầu------");
+                Console.WriteLine("my account:[id:{0},name:{1},balance:{2}]",
+                    my.getid(), my.getname(), my.getbalance());
+                Console.WriteLine("------Thông Tin Tài Khoảng Ban Đầu:[id{0},name{1},balance{2} ",
+                    myfriend.getid(), myfriend.getname(), myfriend.getbalance());
+                Console.WriteLine("------------------------------------------------");
+                // rut tiền 
+                my.debit(2000);
+                myfriend.tranferTo(my,1000);
+
+                Console.WriteLine("------Thông Tin Tài Khoảng sau khi thực hiện giao dịch------");
+                Console.WriteLine("my friend account:[id:{0},name:{1},balance:{2} ",
+                   my.getid(), my.getname(), my.getbalance());
+
+                Console.WriteLine("------Thông Tin Tài Khoảng SAu:[id{0},name{1},balance{2} ",
+                    myfriend.getid(), myfriend.getname(), myfriend.getbalance());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ bai tap 3/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_tap_3
+{
+    class PhanSo
+    {
+        private int tuso;
+        private int mauso;
+        public PhanSo()
+        {
+            tuso = 0;
+            mauso = 1;
+        }
+        public PhanSo(int tuso,int mauso)
+        {
+            this.tuso = tuso;
+            this.mauso = mauso;
+        }
+        public void settuso(int tuso)
+        {
+            this.tuso = tuso;
+        }
+        public int gettuso()
+        {
+            return tuso;
+        }
+        public void setmauso(int mauso)
+        {
+            this.mauso = mauso;
+        }
+        public int getmauo()
+        {
+            return mauso;
+        }
+        public void ToiGian()
+        {
+            int uscln = TimUSCLN(tuso, mauso);
+            tuso = tuso / uscln;
+            mauso = mauso / uscln;
+        }
+        public PhanSo cong(PhanSo PS)
+        {
+            PhanSo Kq = new PhanSo();
+            Kq.tuso = tuso * PS.mauso + PS.tuso * mauso;
+            Kq.mauso = mauso *PS.mauso;
+            return Kq;
+        }
+        public PhanSo Tru(PhanSo PS)
+        {
+            PhanSo Kq = new PhanSo();
+            Kq.tuso = tuso * PS.mauso - PS.tuso * mauso;
+            Kq.mauso = mauso * PS.mauso;
+            return Kq;
+        }
+        public PhanSo Tich(PhanSo PS)
+        {
+            PhanSo Kq = new PhanSo();
+            Kq.tuso = tuso * PS.mauso;
+            Kq.mauso = mauso * PS.mauso;
+            return Kq;
+        }
+
+        public PhanSo Thuong(PhanSo PS)
+        {
+            PhanSo Kq = new PhanSo();          
+            Kq.tuso = tuso * PS.mauso;
+            Kq.mauso = mauso * PS.tuso;
+            return Kq;
+        }
+
+        private int TimUSCLN(int a, int b)
+        {
+            int x = Math.Abs(a);
+            int y = Math.Abs(b);
+            while (x != y) ;
+            {
+                if (x > y)
+                {
+                    x = x - y;
+                }
+                else;
+                y = y - x;
+            }
+            return x;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {               
+                Console.WriteLine("Nhập Tu So:");
+                int tuso = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhập Mau So:");
+                int mauso = int.Parse(Console.ReadLine());
+                PhanSo S1 = new PhanSo(tuso, mauso);
+                S1.settuso(tuso);
+                S1.setmauso(mauso);
+                Console.WriteLine("Nhập Tu So 2:");
+                tuso = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhập Mau So 2:");
+                mauso = int.Parse(Console.ReadLine());
+                PhanSo S2 = new PhanSo(tuso, mauso);
+
+                PhanSo Tong = S1.cong(S2);
+                Console.WriteLine("Xuat Ket Qua:[{0}/{1}+[{2}/[{3}=[{4}/{5}]",
+                    S1.gettuso(),S1.getmauo(),S2.gettuso(),S2.getmauo(),Tong.gettuso(),Tong.getmauo());
+            }
+        }
+    }
+}
